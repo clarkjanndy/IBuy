b21d7ce574d67eb7b887d85890f08ab7c1f17755
clarkjanndy <cjcasol22@gmail.com>
2023-12-26 21:32:26 +0800

    feat: add debug tool urls

--- core/urls.py
+++ core/urls.py
@@ -18,6 +18,7 @@ from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
+import debug_toolbar
 
 
 urlpatterns = [
@@ -27,5 +28,5 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root = settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
+    urlpatterns += path('__debug__/', include(debug_toolbar.urls)),
