2b1058c50722caa63bae060b6c97bcd24c0fe8f2
clarkjanndy <cjcasol22@gmail.com>
2023-12-31 20:31:44 +0800

    feat: add status field for carts

--- backend/models/cart.py
+++ backend/models/cart.py
@@ -8,12 +8,23 @@ def default_expiration_date():
     return timezone.now() + timezone.timedelta(days=7)
 
 class Cart(TimeStampedModel):
+    STATUS = (
+        ('on-cart', 'On-Cart'),
+        ('purchased', 'Purchased'),
+        ('deleted', 'Deleted')
+    )
+    
     uniform = models.ForeignKey('Uniform', related_name='on_cart_items', on_delete=models.CASCADE)
     user = models.ForeignKey('User', related_name='cart_items', on_delete=models.CASCADE)
     variant = models.CharField(max_length=100)
     expiry_date = models.DateTimeField(default=default_expiration_date)
     quantity = models.PositiveIntegerField(default = 1)
-
+    status = models.CharField(choices=STATUS, default='on-cart', max_length=12)
+    
+    def delete(self, *args, **kwargs):
+        self.status = 'on-cart'
+        self.save()
+                
     def __str__(self) -> str:
         return self.uniform.name
     
