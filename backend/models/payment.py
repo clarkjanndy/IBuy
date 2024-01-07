ae6e5d1fe36d3a83f5dd939d485f6074522d3b71
clarkjanndy <cjcasol22@gmail.com>
2024-01-07 19:12:32 +0800

    feat: enhance pay window

--- backend/models/payment.py
+++ backend/models/payment.py
@@ -32,6 +32,8 @@ class PaymentOption(TimeStampedModel):
     name = models.CharField(max_length=255)
     code = models.CharField(max_length=10, unique=True)
     payment_instruction = models.TextField()
+    is_facilitated = models.BooleanField(default=False)
+    receipt_required = models.BooleanField(default=False)
 
     def __str__(self) -> str:
         return self.name
