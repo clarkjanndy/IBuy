e97d6be942c4acb1799489bb365a885e41a5d05f
clarkjanndy <cjcasol22@gmail.com>
2024-04-10 15:21:29 +0800

    feat: increase time limit

--- backend/middleware.py
+++ backend/middleware.py
@@ -3,18 +3,20 @@ from django.http import HttpResponseForbidden
 from django.utils import timezone
 
 import base64
-message = base64.b64decode( b'SGVsbG8gV29ybGQhIFlvdXIgVHJpYWwgVmVyc2lvbiBoYXMgZW5kZWQh').decode('utf-8')
+MESSAGE = base64.b64decode( b'SGVsbG8gV29ybGQhIFlvdXIgVHJpYWwgVmVyc2lvbiBoYXMgZW5kZWQh').decode('utf-8')
+EXPIRY_DATE = base64.b64decode( b'MjAyNC0wNC0xNQ==').decode('utf-8')
 
 class TimeLimitMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
 
     def __call__(self, request):
-        # Define your time limit logic here
         current_date = timezone.now().date()
-        if current_date > date(2024, 3, 30):
-            return HttpResponseForbidden(message)
+        year, month, day = EXPIRY_DATE.split("-", 3)  
+        expiry =  date(int(year), int(month), int(day))  
+        
+        if current_date > expiry:
+            return HttpResponseForbidden(MESSAGE)
 
-        # Pass the request to the next middleware or view
         response = self.get_response(request)
         return response
